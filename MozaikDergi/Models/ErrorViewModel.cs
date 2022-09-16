8e208983370221e3a3d08a16c391f6d856de60ad
Abdullah Yavuz <ayavuz@er-bakir.eris.local>
2022-09-16 15:55:48 +0300

    first commit

---
+++ MozaikDergi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MozaikDergi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
